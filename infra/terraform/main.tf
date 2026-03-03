62734dc5832bc2edfb2dbd784a89d61a0004068f
nolanrhgagnon <nolanrh88@gmail.com>
2026-03-03 19:22:21 +0900

    infra testing

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -104,19 +104,19 @@ resource "aws_lb_target_group_attachment" "app" {
 #  load_balancer_arn = aws_lb.app_alb.arn
 #  port = 80
 #  protocol = "HTTP"
-
+#
 #  default_action {
 #    type = "forward"
 #    target_group_arn = aws_lb_target_group.app_tg.arn
 #  }
 #}
-
+#
 #resource "aws_lb_listener" "https" {
 #  load_balancer_arn = aws_lb.app_alb.arn
 #  port              = 443
 #  protocol          = "HTTPS"
 #  certificate_arn   = aws_acm_certificate.cert.arn
-
+#
 #  default_action {
 #    type             = "forward"
 #    target_group_arn = aws_lb_target_group.app_tg.arn 
