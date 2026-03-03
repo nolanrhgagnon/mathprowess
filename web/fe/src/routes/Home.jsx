9b09ade54a8956022a41c87e605c296454d87c38
nolanrhgagnon <nolanrh88@gmail.com>
2026-03-03 19:54:43 +0900

    mobile friendly?

--- web/fe/src/routes/Home.jsx
+++ web/fe/src/routes/Home.jsx
@@ -11,9 +11,14 @@ const Home = (props) => {
         'Software Engineering',
     ];
     return (
-        <div className="mx-auto w-[80%] flex flex-row">
-            <ConsultationForm />
-            <SubjectList subjects={subjects} />
+        <div className="mx-auto w-full max-w-6xl px-4 flex flex-col gap-6 md:flex-row md:items-start">
+            <div className="w-full md:w-1/2">
+                <ConsultationForm />
+            </div>
+
+            <div className="w-full md:w-1/2">
+                <SubjectList subjects={subjects} />
+            </div>
         </div>
     );
 };
