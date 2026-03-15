1c20964842ed5fe863ce0615a5e9623faf7c8190
nolanrhgagnon <nolanrh88@gmail.com>
2026-03-15 18:03:36 +0900

    vite prerender

--- web/fe/vite.config.js
+++ web/fe/vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import prerender from 'vite-plugin-prerender';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+      react(), 
+      tailwindcss(),
+      prerender({
+        staticDir: 'dist', 
+        routes: ['/', '/about', '/courses'],
+      }),
+  ],
   resolve: {
       alias: {
           '~': path.resolve(__dirname, './src'),
