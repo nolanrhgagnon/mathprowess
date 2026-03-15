2cdecc8529f7457e4531af8c6183692f000ae4f7
nolanrhgagnon <nolanrh88@gmail.com>
2026-03-15 16:50:13 +0900

    django settings

--- api/config/settings.py
+++ api/config/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 
 
 STATIC_URL = "/django_static/"
-STATICFILES_DIRS = [BASE_DIR / "django_static"]
 STATIC_ROOT = BASE_DIR / "django_static"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
