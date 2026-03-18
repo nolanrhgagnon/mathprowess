830aca7fca22d8ac4bdfb78c6197be5af1105410
nolanrhgagnon <nolanrh88@gmail.com>
2026-03-18 18:08:54 +0900

    exposing django metrics via prometheus

--- api/config/urls.py
+++ api/config/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django_prometheus import exports
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("consultations.urls")),
-    path("", include("django_prometheus.urls")),
+    path("api/metrics", exports.ExportToDjangoView, name='prometheus-metrics'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
