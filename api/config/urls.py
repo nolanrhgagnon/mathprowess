b7bc7f2da2c8033ff6263cf25d1134a9c6448714
nolanrhgagnon <nolanrh88@gmail.com>
2026-03-18 19:08:53 +0900

    prometheus looks spec for /metrics on api:8000

--- api/config/urls.py
+++ api/config/urls.py
@@ -7,5 +7,5 @@ from django_prometheus import exports
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("consultations.urls")),
-    path("api/metrics", exports.ExportToDjangoView, name='prometheus-metrics'),
+    path("", include("django_prometheus.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
